77b63b9047d0cfde48750780204de6d2d4aaa948
elizpacific <elizpacific@gmail.com>
2022-11-05 19:42:57 +0200

    added new

--- src/app/code/Mageplaza/Payment/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mageplaza_Payment',
-    __DIR__
-);
-require_once (__DIR__ . '/vendor/autoload.php');
-//require_once 'ginger-php/vendor/autoload.php';
--- src/app/code/Mageplaza/Payment/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Magento_OfflinePayments/js/view/payment/offline-payments':'Mageplaza/view/frontend/web/js/offline-payments',
-        }
-    }
-};
